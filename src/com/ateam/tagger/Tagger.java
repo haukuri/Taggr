c4f25b3fdb2df14fde87ab17b3827415d20fbb5b
Haukur Ingi Heiðarsson <haukur.heidarsson@gmail.com>
2011-02-03 17:29:38 +0000

    Initial commit

---
+++ src/com/ateam/tagger/Tagger.java
@@ -0,0 +1,13 @@
+package com.ateam.tagger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tagger extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
